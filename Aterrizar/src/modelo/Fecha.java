b1d9c92f3e3774a9ef1c0a9fa890a60810b5edf2
AlexisLeandroVallejos <alexv4lle@gmail.com>
2019-05-03 02:44:08 -0300

    Agregado diasDeDiferencia y sus tests.

--- Aterrizar/src/modelo/Fecha.java
+++ Aterrizar/src/modelo/Fecha.java
@@ -1,12 +1,15 @@
 package modelo;
 
 import java.text.ParseException;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import java.util.concurrent.TimeUnit;
+
 import exceptions.FechaInvalidaException;
 
-public class Fecha{
+public class Fecha {
 
 	public static Date convertirAFechaISO8601(String fecha) throws ParseException {
 		Date date = new Date();
@@ -14,7 +17,7 @@ public class Fecha{
 		date = fechaEnFormatoISO8601.parse(fecha);
 		return date;
 	}
-	
+
 	public static Date convertirAFechaLatinoamericana(String fecha) throws ParseException {
 		Date date = new Date();
 		SimpleDateFormat fechaEnFormatoLatinoamericano = new SimpleDateFormat("dd/MM/yyyy");
@@ -27,7 +30,21 @@ public class Fecha{
 		SimpleDateFormat fechaEnFormatoNorteamericano = new SimpleDateFormat("MM-dd-yyyy");
 		date = fechaEnFormatoNorteamericano.parse(fecha);
 		return date;
-		
+
+	}
+
+	// TODO: Flexible debe tener varios formatos para usar y comparar.
+	public static Date convertirAFechaFlexible(String fecha) throws ParseException {
+		Date date = new Date();
+		SimpleDateFormat fechaEnFormatoFlexible = new SimpleDateFormat("MMM dd yyyy");
+		date = fechaEnFormatoFlexible.parse(fecha);
+		return date;
+
+	}
+
+	public static int diasDeDiferencia(Date date1, Date date2) {
+		long date3 = date2.getTime() - date1.getTime();
+		return (int) (date3 / (1000 * 60 * 60 * 24));
 	}
 
 }
--- Aterrizar/src/test/FechaTest.java
+++ Aterrizar/src/test/FechaTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import java.text.ParseException;
+import java.util.Date;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -14,14 +15,14 @@ public class FechaTest {
 	public void convertirAFechaISO8601_fechaEnFormatoISO8601() throws ParseException {
 		String stringDeFecha = "2003-04-12";
 		Fecha fecha = new Fecha();
-		Fecha.convertirAFechaISO8601(stringDeFecha);
+		fecha.convertirAFechaISO8601(stringDeFecha);
 	}
 
 	@Test(expected = ParseException.class)
 	public void convertirAFechaISO8601_fechaQueNoEstaEnFormatoISO8601() throws ParseException {
 		String stringDeFecha = "12/03/1995";
 		Fecha fecha = new Fecha();
-		Fecha.convertirAFechaISO8601(stringDeFecha);
+		fecha.convertirAFechaISO8601(stringDeFecha);
 	}
 
 	// Formato Latinoamericano es: dd/MM/yyyy
@@ -29,14 +30,14 @@ public class FechaTest {
 	public void convertirAFechaLatinoamericana_fechaEnFormatoLatinoamericano() throws ParseException {
 		String stringDeFecha = "10/09/1989";
 		Fecha fecha = new Fecha();
-		Fecha.convertirAFechaLatinoamericana(stringDeFecha);
+		fecha.convertirAFechaLatinoamericana(stringDeFecha);
 	}
 
 	@Test(expected = ParseException.class)
 	public void convertirAFechaLatinoamericana_fechaQueNoEstaEnFormatoLatinoamericano() throws ParseException {
 		String stringDeFecha = "09-12-1989";
 		Fecha fecha = new Fecha();
-		Fecha.convertirAFechaLatinoamericana(stringDeFecha);
+		fecha.convertirAFechaLatinoamericana(stringDeFecha);
 	}
 
 	// Formato Norteamericano es: MM-dd-yyyy
@@ -44,14 +45,49 @@ public class FechaTest {
 	public void convertirAFechaNorteamericana_fechaEnFormatoNorteamericano() throws ParseException {
 		String stringDeFecha = "12-24-2015";
 		Fecha fecha = new Fecha();
-		Fecha.convertirAFechaNorteamericana(stringDeFecha);
+		fecha.convertirAFechaNorteamericana(stringDeFecha);
 	}
 
 	@Test(expected = ParseException.class)
 	public void convertirAFechaNorteamericana_fechaQueNoEstaEnFormatoNorteamericano() throws ParseException {
 		String stringDeFecha = "12/07/1995";
 		Fecha fecha = new Fecha();
-		Fecha.convertirAFechaNorteamericana(stringDeFecha);
+		fecha.convertirAFechaNorteamericana(stringDeFecha);
+	}
+
+	// TODO: Formato Flexible es: "MMM dd yyyy" y otros mas...
+	@Test
+	public void convertirAFechaFlexible_fechaEnFormatoFlexible() throws ParseException {
+		String stringDeFecha = "Oct 12 2013";
+		Fecha fecha = new Fecha();
+		fecha.convertirAFechaFlexible(stringDeFecha);
+
+	}
+
+	@Test(expected = ParseException.class)
+	public void convertirAFechaFlexible_fechaQueNoEstaEnFormatoFlexible() throws ParseException {
+		String stringDeFecha = "Apr-1-2002";
+		Fecha fecha = new Fecha();
+		fecha.convertirAFechaFlexible(stringDeFecha);
+
+	}
+
+	@Test
+	public void diasDeDiferencia_dosFechasQueDanQuinceDiasDeDiferencia() {
+		Date date1 = new Date(1995,12,1);
+		Date date2 = new Date(1995,12,16);
+		Fecha fecha = new Fecha();
+		Assert.assertSame(fecha.diasDeDiferencia(date1, date2), 15);
+
+	}
+	
+	@Test
+	public void diasDeDiferencia_dosFechasQueNoDanQuinceDiasDeDiferencia() {
+		Date date1 = new Date(1995,12,1);
+		Date date2 = new Date(1997,7,20);
+		Fecha fecha = new Fecha();
+		Assert.assertNotSame(fecha.diasDeDiferencia(date1, date2), 15);
+
 	}
 
 }
